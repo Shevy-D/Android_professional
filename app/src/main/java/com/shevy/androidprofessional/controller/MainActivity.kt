4218c53f943cc48eded293f819c8d19b2a287ddf
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-06-23 21:58:08 +0500

    Task #2

--- app/src/main/java/com/shevy/androidprofessional/controller/MainActivity.kt
+++ app/src/main/java/com/shevy/androidprofessional/controller/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import com.shevy.androidprofessional.R
@@ -13,7 +14,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var lastButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -33,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_btn)
         falseButton = findViewById(R.id.false_btn)
         nextButton = findViewById(R.id.next_btn)
+        lastButton = findViewById(R.id.last_btn)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
@@ -46,6 +49,13 @@ class MainActivity : AppCompatActivity() {
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
+        }
+
+        lastButton.setOnClickListener {
+            if (currentIndex != 0) {
+                currentIndex = (currentIndex - 1) % questionBank.size
+                updateQuestion()
+            }
         }
 
         questionTextView.setOnClickListener {
