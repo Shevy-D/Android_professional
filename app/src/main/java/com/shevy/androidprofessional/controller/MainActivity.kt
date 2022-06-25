901f12f8e21033a4b8e568c882f1164a8307bbac
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-06-25 21:27:08 +0500

    Lesson 4

--- app/src/main/java/com/shevy/androidprofessional/controller/MainActivity.kt
+++ app/src/main/java/com/shevy/androidprofessional/controller/MainActivity.kt
@@ -2,12 +2,19 @@ package com.shevy.androidprofessional.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.shevy.androidprofessional.QuizViewModel
 import com.shevy.androidprofessional.R
 import com.shevy.androidprofessional.model.Question
+
+private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
+/*    private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
         Question(R.string.question_mideast, false),
@@ -24,11 +31,18 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_america, true),
         Question(R.string.question_asia, true)
     )
-    private var currentIndex = 0
+    private var currentIndex = 0*/
+
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_btn)
         falseButton = findViewById(R.id.false_btn)
@@ -44,7 +58,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
@@ -53,18 +67,32 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }*/
 
-        val questionTextResId = getString(questionBank[currentIndex].textResId)
+        val questionTextResId = getString(quizViewModel.currentQuestionText)
         questionTextView.text = questionTextResId
 
     }
 
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
     private fun updateQuestion() {
-        val questionTextResId = getString(questionBank[currentIndex].textResId)
+        val questionTextResId = getString(quizViewModel.currentQuestionText)
         questionTextView.text = questionTextResId
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         val messageResId = if (userAnswer == correctAnswer) {
             getString(R.string.correct_toast)
         } else {
