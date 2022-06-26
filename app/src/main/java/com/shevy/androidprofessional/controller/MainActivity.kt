27149984ff12c597cbc725756e9be4af28ee0a30
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-06-26 17:04:40 +0500

    Lesson 6

--- app/src/main/java/com/shevy/androidprofessional/controller/MainActivity.kt
+++ app/src/main/java/com/shevy/androidprofessional/controller/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.shevy.androidprofessional.controller
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -12,6 +14,7 @@ import com.shevy.androidprofessional.R
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
+    private lateinit var cheatButton: Button
 
 /*    private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -44,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_btn)
         falseButton = findViewById(R.id.false_btn)
         nextButton = findViewById(R.id.next_btn)
+        cheatButton = findViewById(R.id.cheat_btn)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
@@ -59,6 +64,16 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        cheatButton.setOnClickListener {
+            //val intent = Intent(this, CheatActivity::class.java)
+
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            //startActivity(intent)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+        }
+        updateQuestion()
+
 /*        questionTextView.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
@@ -66,7 +81,16 @@ class MainActivity : AppCompatActivity() {
 
         val questionTextResId = getString(quizViewModel.currentQuestionText)
         questionTextView.text = questionTextResId
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            quizViewModel.isCheater = data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
     }
 
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
@@ -82,10 +106,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = quizViewModel.currentQuestionAnswer
-        val messageResId = if (userAnswer == correctAnswer) {
+/*        val messageResId = if (userAnswer == correctAnswer) {
             getString(R.string.correct_toast)
         } else {
             getString(R.string.incorrect_toast)
+        }*/
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
