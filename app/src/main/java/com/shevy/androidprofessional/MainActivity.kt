0cc1b0dcfe88ac094cc05e173f74929658c8775e
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-07-09 16:27:25 +0500

    Finished the lesson #8

--- app/src/main/java/com/shevy/androidprofessional/MainActivity.kt
+++ app/src/main/java/com/shevy/androidprofessional/MainActivity.kt
@@ -8,6 +8,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_crime)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
